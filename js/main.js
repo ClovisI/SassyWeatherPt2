d07fd8a005d743a52c336bca1873f1710d264ec7
Patrick <pgronstal@hotmail.com>
2017-07-20 08:55:11 -0600

    First of last version of weather api

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const apiURL = "https://uwpce-weather-proxy.herokuapp.com/data/2.5/weather";
+const apiID = "07b70a15018f9b029bad197517f7d755";
+const units = imperial;
+
+var cities = [
+	{ lat:51.5074, lon: 0.1278}, // london
+	{ lat: 47.6762, lon: -122.3182 } //seattle
+];
+
+
+function whatsWeather(lat,long){
+	var locationWeather =new XMLHttpRequest();
+	locationWeather.open("GET", ,true);
+	locationWeather.send();
+
+api.openweathermap.org/data/2.5/weather?lat=35&lon=1390&units=units&appid=apiURL;
+};
+
+api.openweathermap.org/data/2.5/weather?lat=35&lon=139
